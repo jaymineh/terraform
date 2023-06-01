76d2b7cee758f19e11224d4b676091c5a826ef60
Jaymineh <91850543+jaymineh@users.noreply.github.com>
2023-06-01 22:19:42 +0100

    Create main.tf for variables directory

---
+++ variables/main.tf
@@ -0,0 +1,43 @@
+variable "vpcname" {
+  type = string
+  default = "myvpc"
+}
+
+variable "sshport" {
+  type = number
+  default = 22
+}
+
+variable "enabled" {
+    default = true
+}
+
+variable "mylist" {
+  type = list(string)
+  default = ["value1","value2"]
+}
+
+variable "mymap" {
+  type = map
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "JMNvpc" {
+ cidr_block = "18.0.0.0/24"
+
+ tags = {
+   Name = var.inputname
+ }
+}
+
+variable "inputname" {
+  type = string
+  description = "Set the name of the VPC"
+}
