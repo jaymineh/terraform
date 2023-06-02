cfecdd27f243c0a3436099a8db869890437ed6bb
Jaymineh <91850543+jaymineh@users.noreply.github.com>
2023-06-02 12:07:24 +0100

    Update main.tf

--- variables/main.tf
+++ variables/main.tf
@@ -41,3 +41,20 @@ variable "inputname" {
   type = string
   description = "Set the name of the VPC"
 }
+
+output "vpcid" {
+    value = aws_vpc.JMNvpc.id
+}
+
+variable "mytuple" {
+  type = tuple([string, number, string])
+  default = ["cat", 1, "dog"]
+}
+
+variable "myobject" {
+    type = object({name = string, port = list(number)})
+    default = {
+        name = "JMN"
+        port = [22, 25, 80]
+    }
+}
