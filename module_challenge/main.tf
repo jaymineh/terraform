7620185ea27a9b4f10bab8620fdefca1fb201082
Jaymineh <91850543+jaymineh@users.noreply.github.com>
2023-06-28 16:51:10 +0100

    Updated main.tf with config

--- module_challenge/main.tf
+++ module_challenge/main.tf
@@ -1,1 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+module "webmodule" {
+  source = "./web"
+  ec2name = "Webserver"
+}
+
+output "web_output" {
+  value = module.webmodule.instance_id
+}
+
+module "dbmodule" {
+  source = "./db"
+  ec2name = "Database"
+}
+
+output "db_output" {
+    value = module.dbmodule.instance_id
+}
+
+output "web_IP" {
+    value = module.webmodule.PublicIP
+}
+
+output "db_IP" {
+  value = module.dbmodule.PrivateIP
+}
